5b06b83c9012faa93a5c0249a9ef16892eb178a3
索马里水稻 <2962817993@qq.com>
2025-06-11 13:51:50 +0800

    日志工具类

---
+++ my_test.py
@@ -0,0 +1,15 @@
+import logging
+
+
+class Logutil:
+    def __init__(self):
+        self.log = logging.getLogger("坤坤")
+        self.log.setLevel(logging.INFO)
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.INFO)
+        sh.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+        fh=logging.FileHandler("zzc.log", "a", encoding="utf-8")
+        fh.setLevel(logging.WARNING)
+        fh.setFormatter(logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s"))
+        self.log.addHandler(sh)
+        self.log.addHandler(fh)
