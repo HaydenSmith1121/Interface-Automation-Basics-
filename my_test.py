f755610c03683b1f41215f4c590d67f53af477d0
索马里水稻 <2962817993@qq.com>
2025-06-11 14:53:10 +0800

    日志工具类

--- my_test.py
+++ my_test.py
@@ -8,8 +8,10 @@ class Logutil:
         sh = logging.StreamHandler()
         sh.setLevel(logging.INFO)
         sh.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-        fh=logging.FileHandler("zzc.log", "a", encoding="utf-8")
+        fh = logging.FileHandler("zzc.log", "a", encoding="utf-8")
         fh.setLevel(logging.WARNING)
         fh.setFormatter(logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s"))
+        fh.close()
         self.log.addHandler(sh)
         self.log.addHandler(fh)
+
