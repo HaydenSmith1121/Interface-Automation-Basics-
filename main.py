4451b50eede22845e5a97a2935764fc3143a8b76
索马里水稻 <2962817993@qq.com>
2025-06-12 19:18:08 +0800

    程序执行入口

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import pytest
 
 if __name__ == '__main__':
     # 用例文件的位置和报告文件的位置都是相对于当前main文件而言的
-    pytest.main(["-sv", "test_case/test_06.py", '--html=report/my_report_05.html'])
+    pytest.main(["-sv", "test_case/test_07.py", '--html=report/my_report_07.html'])
