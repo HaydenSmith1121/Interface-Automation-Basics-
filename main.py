0137541d2618986d690378ebb75f609a2f4eca2d
索马里水稻 <2962817993@qq.com>
2025-06-12 17:22:54 +0800

    测试数据

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import pytest
 
 if __name__ == '__main__':
     # 用例文件的位置和报告文件的位置都是相对于当前main文件而言的
-    pytest.main(["-sv", "test_case/test_03.py", '--html=report/my_report_02.html'])
+    pytest.main(["-sv", "test_case/test_06.py", '--html=report/my_report_05.html'])
