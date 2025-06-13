6ad0dbd612bd5be536306f1baf15966b1ba96dad
索马里水稻 <2962817993@qq.com>
2025-06-13 12:56:41 +0800

    程序的执行入口，pytest相关的配置文件必须放在同一级目录或者子目录中

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pytest
 
 if __name__ == '__main__':
-    # 用例文件的位置和报告文件的位置都是相对于当前main文件而言的
-    pytest.main(["-sv", "test_case/test_07.py", '--html=report/my_report_07.html'])
+    pytest.main(["-sv", "test_case/test_11.py", '--html=report/my_report_11.html'])
